9fa3fec2f6e6807c94a4c30d70fb1fa250ecc368
Mikołaj Barcik <s22561@pjwstk.edu.pl>
2023-12-19 09:50:28 +0100

    Created branch, streamlit and sliders

--- src/asi/__main__.py
+++ src/asi/__main__.py
@@ -42,6 +42,27 @@ def main(*args, **kwargs):
     run = _find_run_command(package_name)
     run(*args, **kwargs)
 
+    st.title("Rain prediction app")
+
+    minTemp_slider = st.slider("Min temperature", min_value=-10, max_value=50, step=0.5)
+    maxTemp_slider = st.slider("Max temperature", min_value=-10, max_value=50, step=0.5)
+    rainfall_slider = st.slider("Rainfall", min_value=0, max_value=400, step=1)
+    evaporation_slider = st.slider("Evaporation", min_value=0, max_valute=150, step=1)
+    sunshine_slider = st.slider("Sunshine", min_value=0, max_value=15, step=1)
+    windGustSpeed_slider = st.slider("Wind gust speed", min_value=0, max_value=140, step=1)
+    windSpeed9am_slider = st.slider("Wind speed at 9am", min_value=0, max_value=140, step=1)
+    windSpeed3pm_slider = st.slider("Wind speed at 3am", min_value=0, max_value=140, step=1)
+    humidity9am_slider = st.slider("Humidity at 9am", min_value=0, max_value=100, step=1)
+    humidity3pm_slider = st.slider("Humidity at 3pm", min_value=0, max_value=100, step=1)
+    pressure9am_slider = st.slider("Pressure at 9am", min_value=970, max_value=1050, step=0.5)
+    pressure3pm_slider = st.slider("Pressure at 3pm", min_value=970, max_value=1050, step=0.5)
+    cloud9am_slider = st.slider("Cloudiness at 9am", min_value=0, max_value=10, step=1)
+    cloud3pm_slider = st.slider("Cloudiness at 3pm", min_value=0, max_value=10, step=1)
+    temp9am_slider = st.slider("Temperature at 9am", min_value=-10, max_value=50, step=1)
+    temp3pm_slider = st.slider("Temperature at 3pm", min_value=-10, max_value=50, step=1)
+
+
+
 
 if __name__ == "__main__":
     main()
